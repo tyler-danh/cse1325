f95c91c6faf1eae3be0e086dd68028570c793e27
Tyler Danh <txd6321@mavs.uta.edu>
2023-01-20 11:03:24 -0600

    EXTREME BONUS!

---
+++ P01/extreme_bonus/Hello.java
@@ -0,0 +1,10 @@
+import java.net.InetAddress;
+
+public class Hello{
+	public static void main(String[] args){
+		
+		String Name = System.getProperty("user.name");
+		
+		System.out.println("Hello, " + Name + "!");
+	}
+}
