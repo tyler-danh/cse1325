3ef02b1afea2c81f15b3afdf1bfc41b2a93fd4cf
Tyler Danh <txd6321@mavs.uta.edu>
2023-01-20 11:10:02 -0600

    bonus!

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -7,6 +7,6 @@ public class Hello{
 		Scanner input = new Scanner(System.in);
 		System.out.println("What is your name?");
 		name = input.nextLine();
-		System.out.println("Hello " + (name) + "!");
+		System.out.println("Hello, " + (name) + "!");
 	}
 }
