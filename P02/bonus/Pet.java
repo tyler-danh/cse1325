e19c3a271dd2e7a8c1f7b7c27dcaa34c99062484
Tyler Danh <txd6321@mavs.uta.edu>
2023-01-29 01:30:54 -0600

    bonus!

---
+++ P02/bonus/Pet.java
@@ -0,0 +1,17 @@
+
+public class Pet{
+	private String name;
+	private double age;
+	private Type type;
+	
+	public Pet(String name, double age, Type type){
+		this.name = name;
+		this.age = age;
+		this.type = type;
+	}
+	
+	@Override
+	public String toString(){
+		return name + " is a " + type + " age: " + age;
+	}
+}
---
+++ P02/bonus/Type.java
@@ -0,0 +1,5 @@
+public enum Type{
+	Dog(13), Raccoon(3), Cat(14), Robin(2);
+	
+	
+}
