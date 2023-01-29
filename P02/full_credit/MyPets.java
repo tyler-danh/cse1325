e435c82c9d23651515442b96c589c6e86c659831
Tyler Danh <txd6321@mavs.uta.edu>
2023-01-29 01:20:20 -0600

    full credit!

--- P02/full_credit/MyPets.java
+++ P02/full_credit/MyPets.java
@@ -6,9 +6,9 @@ public class MyPets{
 		Pet[] animal = new Pet[4];
 		
 		animal[0] = new Pet("Fluffy", 10, Type.Dog);
-		animal[1] = new Pet("Blessedone", 2, Type.Raccoon);
+		animal[1] = new Pet("Rocket", 2, Type.Raccoon);
 		animal[2] = new Pet("Millie", 7, Type.Cat);
-		animal[3] = new Pet("Enna", 8, Type.Songbird);
+		animal[3] = new Pet("Enna", 8, Type.Robin);
 		
 		for(int i=0; i<4 ; ++i){
 			System.out.println(animal[i]);
