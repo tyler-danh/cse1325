77bfdd25a0efdcba9423ad0e1c149525f2126e05
Tyler Danh <txd6321@mavs.uta.edu>
2023-02-05 15:22:14 -0600

    full credit!

--- P03/full_credit/Quiz.java
+++ P03/full_credit/Quiz.java
@@ -1,7 +1,11 @@
 public class Quiz{
     private ArrayList<Question> questions = new ArrayList<>();
 
-    public Quiz(Question questions){
-        
+    public Quiz(ArrayList<Question> questions){
+        this.questions = questions;
+    }
+
+    public double takeQuiz(){
+        questions.add("How old is the Earth?", )
     }
 }
