13d2fef972a87310963fc76d2e9ae037057072be
Tyler Danh <txd6321@mavs.uta.edu>
2023-02-08 10:50:28 -0600

    full credit

--- P04/full_credit/Product.java
+++ P04/full_credit/Product.java
@@ -1,4 +1,20 @@
 public class Product{
+    protected double cost;
+    protected String name;
 
-    
+    public class Product{
+        this.cost = cost;
+        this.name = name;
+
+        if(cost<0){
+            throw new IllegalArgumentException("Cost: " + cost + " cannot be negative!");
+        }
+    }
+    public abstract class price{}
+
+    public string toString() {
+        StringBuilder item = new Stringbuilder("" + name + "(" + cost + ")" + "" + cost);
+        return item.toString();
+    }
+
 }
