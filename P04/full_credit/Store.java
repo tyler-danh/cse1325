62e205ec4337d53196439091eb689da3e3ca95d6
Tyler Danh <txd6321@mavs.uta.edu>
2023-02-10 11:24:34 -0600

    full credit

--- P04/full_credit/Store.java
+++ P04/full_credit/Store.java
@@ -12,7 +12,7 @@ public class Store{
         
     }
     private ArrayList<Product> products = new ArrayList<>(
-            Arrays.asList(new Taxed("Coca-cola (2-liter)", 2.68), new Taxed("Cookies & Cream Icecream (half gallon)", 7.98), new Taxed("M&Ms Milk Chocolate (family size)", 6.98),
+            Arrays.asList(new Taxed("Coca-cola (2-liter)", 2.68), new Taxed("Cookies & Cream Ice Cream (half gallon)", 7.98), new Taxed("M&Ms Milk Chocolate (family size)", 6.98),
                             new TaxFree("Milk (1-gallon)", 3.07), new TaxFree("Bread", 3.13), new TaxFree("Butter", 4.98)));
 
     private ArrayList<Product> shoppingCart = new ArrayList<>();
