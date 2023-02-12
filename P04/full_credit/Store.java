21ca5d4d97457be0be1753aa1c3797dbc605353d
Tyler Danh <txd6321@mavs.uta.edu>
2023-02-12 00:59:27 -0600

    full credit

--- P04/full_credit/Store.java
+++ P04/full_credit/Store.java
@@ -5,15 +5,27 @@ import java.util.Scanner;
 
 public class Store{
     public static void main(String[] args){
-        setSalesTax();
-        System.out.println("= = = = = = = = = = = = = = = = = = = =\n" 
-                                + "   Welcome to WAHMart    \n"
+        Taxed.setTaxRate(0.08);
+        Scanner input = new Scanner(System.in);
+
+        while(true){
+            System.out.println("= = = = = = = = = = = = = = = = = = = =\n" 
+                                + "          Welcome to WAHMart    \n"
                         + "= = = = = = = = = = = = = = = = = = = =\n");
+            for(Product p : products){
+                int i = 1;
+                p.price();
+                //System.out.printf("%d) %.2f\n", ++i , p.price());
+
+                System.out.println(i + p.toString());
+                ++i;
+            }
+        }
         
     }
-    private ArrayList<Product> products = new ArrayList<>(
-            Arrays.asList(new Taxed("Coca-cola (2-liter)", 2.68), new Taxed("Cookies & Cream Ice Cream (half gallon)", 7.98), new Taxed("M&Ms Milk Chocolate (family size)", 6.98),
+    private static ArrayList<Product> products = new ArrayList<>(
+           Arrays.asList(new Taxed("Coca-cola (2-liter)", 2.68), new Taxed("Cookies & Cream Ice Cream (half gallon)", 7.98), new Taxed("M&Ms Milk Chocolate (family size)", 6.98),
                             new TaxFree("Milk (1-gallon)", 3.07), new TaxFree("Bread", 3.13), new TaxFree("Butter", 4.98)));
 
-    private ArrayList<Product> shoppingCart = new ArrayList<>();
+    private static ArrayList<Product> shoppingCart = new ArrayList<>();
 }
