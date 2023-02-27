0bb9d3150b570968dae6631acb82b3868d73f8a6
Tyler Danh <txd6321@mavs.uta.edu>
2023-02-27 12:54:59 -0600

    SPRINT1

--- P05/Order.java
+++ P05/Order.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Order{
     public Order(Customer customer){
         this.customer = customer;
-        this.orderNumber = nextOrderNumber++;
+        this.orderNumber = nextOrderNumber;
     }
 
     public void addComputer(Computer computer){
@@ -19,15 +19,15 @@ public class Order{
         return result.toString();
     }
 
-    @Override
-    public boolean equals(Object o){
+    //@Override
+    /*public boolean equals(Object o){
         if(this == o)
             return true;
         if((o == null) || !(o instanceof Order))
             return false;
         Order ord = (Order) o;
         return ();
-    }
+    }*/
 
     private static long nextOrderNumber = 0;
     private long orderNumber;
